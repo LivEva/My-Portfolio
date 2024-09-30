0eb15665bb960806966cb828a4a1f88ea149e274
LivEva <guests@hotmail.co.uk>
2024-09-30 12:42:22 +0100

    Adding new plant project component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomeScreen from './components/screens/HomeScreen';
 import NavBar from './components/main/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProjectLists from './components/screens/ProjectsList';
+import PlantAppPage from './components/screens/PlantAppPage';
 
 
 function App() {
@@ -22,6 +23,8 @@ function App() {
      <Route path='/Home' element={<HomeScreen />}/>
 
      <Route path='/Projects' element={<ProjectLists />}/>
+
+     <Route path='/MyPlantApp' element={<PlantAppPage/>}/>
 
     </Routes>
 
---
+++ src/components/screens/PlantAppPage.jsx
@@ -0,0 +1,15 @@
+const PlantAppPage = () => {
+
+    return (
+
+        <>
+
+        <h1>My Plant App</h1>
+        
+        </>
+    )
+
+
+}
+
+export default PlantAppPage;
