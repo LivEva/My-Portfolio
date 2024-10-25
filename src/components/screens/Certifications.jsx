baa9244fca7912d00255a27f6e7b3ce38da80921
LivEva <guests@hotmail.co.uk>
2024-10-25 11:10:21 +0100

    Adding new certifications content and styling

--- src/components/screens/Certifications.jsx
+++ src/components/screens/Certifications.jsx
@@ -1,6 +1,27 @@
+import '../../CSS/certificationsPage.css'
+
+
 const Certifications = () => {
 
-    return <h1>Certifications</h1>
+    return (
+
+<div className='certification-container'>
+    
+    <h2 className="title">Certifications</h2>
+
+    <div className='certifications'>
+
+    <img className='certificate-image' src="/src/assets/certificate.png" alt="" />
+
+    <img className='certificate-image' src="/src/assets/certificate.png" alt="" />
+
+    <img className='certificate-image' src="/src/assets/certificate.png" alt="" />
+
+    </div>
+
+</div>
+
+    )
 
 }
 
