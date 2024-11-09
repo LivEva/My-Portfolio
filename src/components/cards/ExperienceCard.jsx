5fef1fbdc2e754de0a60bc380968832bed06af53
LivEva <guests@hotmail.co.uk>
2024-11-09 16:09:23 +0000

    Adding additional styling

--- src/components/cards/ExperienceCard.jsx
+++ src/components/cards/ExperienceCard.jsx
@@ -7,9 +7,9 @@ const ExperienceCard = () => {
     
              <h2>Experience</h2>
     
-            <section className="bio">
+            <section className="experience">
             <ul>
-            <h3>Ward Clerk - Salford Royal Hospital NHS</h3>
+            <h3>Ward Clerk - IFU Ward - Salford Royal - NHS</h3>
             
             <li>My role on the Intestinal Failure Unit was highly team-orientated, requiring me to communicate with a range of medical professionals daily. </li>
             <li>My role involved effectively dealing with all enquiries, via telephone and face to face regarding patients, staff, internal and external agencies. I maintained daily contact with colleagues working within the team to ensure priorities were met and targets achieved, whilst actively seeking ways to improve the service.
