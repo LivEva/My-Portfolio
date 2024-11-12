f915e87f5f20d06688826377b154c6cafcf09bd7
LivEva <guests@hotmail.co.uk>
2024-11-12 14:11:26 +0000

    Adding updated app info and link

--- src/components/cards/PlantAppPageContent.jsx
+++ src/components/cards/PlantAppPageContent.jsx
@@ -1,6 +1,7 @@
 import '../../CSS/singleProjects.css'
 import reactLogo from '../../assets/reactLogo.png'
 import plantApp from '../../assets/plantAppWelcome.png'
+import firebaseLogo from '../../assets/firebaseLogo.png'
 
 const PlantAppPageContent = ({ appInfo }) => {
 
@@ -30,6 +31,8 @@ const PlantAppPageContent = ({ appInfo }) => {
 
                     <p>View project: <a href="https://www.youtube.com/watch?v=3zPU6vK9B9M&t=1s"> {appInfo.webAddress}</a></p>
 
+                    <p>Additional Information: <a href="https://northcoders.com/project-phase/my-plants-app">https://northcoders.com/project-phase/my-plants-app</a></p>
+
                 </section>
             
             <section className="logos-container">
@@ -38,7 +41,7 @@ const PlantAppPageContent = ({ appInfo }) => {
 
                 <img src={reactLogo} alt="react native logo" className="logo" />
 
-                <img src="..." alt="firebase logo" className="logo" />
+                <img src={firebaseLogo} alt="firebase logo" className="logo" />
 
                 <img src="https://www.svgrepo.com/show/354090/mongodb.svg" alt="mongoDb logo" className="logo" />
 
