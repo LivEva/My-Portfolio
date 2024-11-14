be2645b4decc0cf4411381959cd1ba5604dad0d6
LivEva <guests@hotmail.co.uk>
2024-11-14 12:40:26 +0000

    Correcting spelling error

--- src/components/cards/PlantAppCard.jsx
+++ src/components/cards/PlantAppCard.jsx
@@ -10,7 +10,7 @@ const PlantAppCard = () => {
         <Link to='/MyPlantApp'><img src={plantsWallpaper} alt="Image of project" className="image"/></Link>
         <h2>My Plant App</h2>
         <h3>Mobile app (IOS & Android)</h3>
-        <p>"An app designed for casual plant owners and botonists alike."</p>
+        <p>"An app designed for casual plant owners and botanists alike."</p>
 
         </div>
         
